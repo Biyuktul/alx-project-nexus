543a760909b16116ede16ff04d31f7ec00a9bcb6
Yonatan <yonatanaddis0257@gmail.com>
2025-07-23 19:34:41 +0300

    feat: integrate Swagger for API documentation

--- job_board_backend/job_board_backend/settings.py
+++ job_board_backend/job_board_backend/settings.py
@@ -28,6 +28,8 @@ INSTALLED_APPS = [
     'drf_yasg',
 ]
 
+AUTH_USER_MODEL = 'authentication.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
