f492148a855adcb60d19abae97b52d2e3e6cce7b
Yonatan <yonatanaddis0257@gmail.com>
2025-07-25 20:41:42 +0300

    feat: implement job Application and Application managment APIs

--- job_board_backend/job_applications/models.py
+++ job_board_backend/job_applications/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Application(models.Model):
+    application_status = (
+        ('submited', 'Submited'),
+        ('rejected', 'Rejected'),
+        ('shortlisted', 'Shortlisted'),
+        ('accepted', 'Accepted'),
+    )
+    applicant = models.ForeignKey('authentication.User', on_delete=models.CASCADE)
+    job = models.ForeignKey('jobs.Job', on_delete=models.CASCADE)
+    application_letter = models.TextField()
+    status = models.CharField(max_length=50, choices=application_status)
+    application_time = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ['applicant', 'job']
+    
+    def __str__(self):
+        return f"{self.applicant.username} - {self.job.title}"
