07067fc72e15f6df12b14f76e76d7af9fe3f25ad
Yonatan <yonatanaddis0257@gmail.com>
2025-08-03 14:24:01 +0300

    perf: adding index's on some columns to improve performance

--- job_board_backend/jobs/models.py
+++ job_board_backend/jobs/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, db_index=True)
     description = models.TextField(blank=True, help_text="Brief description of the category")
 
     def __str__(self):
@@ -15,13 +15,18 @@ class Job(models.Model):
     )
     title = models.CharField(max_length=200)
     description = models.TextField()
-    location = models.CharField(max_length=100)
+    location = models.CharField(max_length=100, db_index=True)
     salary = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     type = models.CharField(max_length=20, choices=types)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='jobs')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='jobs', db_index=True)
     recruiter = models.ForeignKey('authentication.User', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return self.title
+
+    class Meta:
+          indexes = [
+              models.Index(fields=['category', 'location'], name='job_category_location_idx'),
+          ]
